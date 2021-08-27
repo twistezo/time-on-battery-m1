6bcb2dba3aa3eb7f843e7a4baf4af29135d10876
Łukasz Kółko <kolkolukasz@gmail.com>
2021-08-27 22:45:20 +0200

    ponglish

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# time-on-battery-m1'
+# time-on-battery-m1
 
 Mac OS app for measure real elapsed time on battery between charging. Without sleeping time unlike system's Activity Monitor counts.
 
--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const appName = 'time-on-battery-m1'
 export const description = `Mac OS app for measure real elapsed time on battery between charging.
-Without sleeping time unlike system's Activity Monitor counts.
+Without sleeping time unlike in system's Activity Monitor.
 Requires Big Sur and M1 processor.
 `
 export const isChargingCmd = `system_profiler SPPowerDataType | grep Connected | awk '{print $2}'`
